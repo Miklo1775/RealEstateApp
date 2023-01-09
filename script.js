0a05f68b87f7ab59fed211b4a7afa0e331d3724c
Victor <thelegendofvictor@gmail.com>
2023-01-09 10:52:04 -0600

    made changes to 1 file

---
+++ script.js
@@ -0,0 +1,7 @@
+const nav = document.querySelector(".navbar");
+const address = document.querySelector(".address");
+const propertyType = document.querySelector(".property-type");
+const city = document.querySelector(".city");
+const state = document.querySelector(".state");
+const ul = document.querySelector(".array");
+const btnSubmit = document.querySelector(".btn-submit");
